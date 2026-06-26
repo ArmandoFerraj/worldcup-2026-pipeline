7bb5597111e8f45045e15a62930d333aaf4bb0b2
ArmandoFerraj <aferraj22@gmail.com>
2026-06-26 14:18:10 -0400

    added logging to run_pipeline.sh script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,6 +2,9 @@
 
 #world cup pipeline: extract -> load -> transform
 
+# Redirect all output (stdout + errors) to a log file
+exec >> /home/ubuntu/pipeline.log 2>&1
+
 cd /home/ubuntu/worldcup-2026-pipeline
 source venv/bin/activate
 
